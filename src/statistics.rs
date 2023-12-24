3996be4324ec72a3a9a7b6e09a26b9b4ea8e3924
Banyc <36535895+Banyc@users.noreply.github.com>
2023-12-25 01:10:17 +0800

    fix: fallible distance

--- src/statistics.rs
+++ src/statistics.rs
@@ -1,4 +1,4 @@
-use std::num::NonZeroUsize;
+use std::num::NonZeroU32;
 
 use strict_num::{FiniteF64, PositiveF64};
 use thiserror::Error;
@@ -67,7 +67,10 @@ where
 }
 
 pub trait DistanceExt: Iterator<Item = (f64, f64)> + Sized {
-    fn distance(self, p: NonZeroUsize) -> f64 {
+    /// # Panic
+    ///
+    /// If `p` cannot be converted into `i32`.
+    fn distance(self, p: NonZeroU32) -> f64 {
         let p_i32 = i32::try_from(p.get()).unwrap();
         let sum = self.map(|(a, b)| (a - b).abs().powi(p_i32)).sum::<f64>();
         match p.get() {
@@ -84,12 +87,18 @@ pub trait DistanceExt: Iterator<Item = (f64, f64)> + Sized {
 impl<T: Iterator<Item = (f64, f64)>> DistanceExt for T {}
 
 pub trait FiniteDistanceExt: Iterator<Item = (FiniteF64, FiniteF64)> + Sized {
-    fn distance(self, p: NonZeroUsize) -> FiniteF64 {
+    /// # Panic
+    ///
+    /// If `p` cannot be converted into `i32`.
+    fn distance(self, p: NonZeroU32) -> Result<FiniteF64, InfiniteDistanceError> {
         let d = self.map(|(a, b)| (a.get(), b.get())).distance(p);
-        FiniteF64::new(d).unwrap()
+        FiniteF64::new(d).ok_or(InfiniteDistanceError)
     }
 }
 impl<T: Iterator<Item = (FiniteF64, FiniteF64)>> FiniteDistanceExt for T {}
+#[derive(Debug, Error, Clone, Copy)]
+#[error("Infinite distance")]
+pub struct InfiniteDistanceError;
 
 #[cfg(test)]
 mod tests {
@@ -103,5 +112,18 @@ mod tests {
 
         let empty: [f64; 0] = [];
         assert!(empty.iter().copied().mean().is_err());
+    }
+
+    #[test]
+    fn test_distance() {
+        let polarized = [(
+            FiniteF64::new(f64::MIN).unwrap(),
+            FiniteF64::new(f64::MAX).unwrap(),
+        )];
+        let distance = polarized
+            .iter()
+            .copied()
+            .distance(NonZeroU32::new(2).unwrap());
+        assert!(distance.is_err());
     }
 }

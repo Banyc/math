82a3362717b8cbcf38819b158084d6d8da48bfd9
Banyc <36535895+Banyc@users.noreply.github.com>
2024-09-22 14:27:02 +0800

    refactor: defensive programming

--- src/vector.rs
+++ src/vector.rs
@@ -7,54 +7,65 @@ use crate::{
     matrix::{self, Container2D},
 };
 
-#[derive(Debug, Clone, Copy)]
-pub struct Vector<const N: usize> {
-    dims: [FiniteF64; N],
+pub use minimal::*;
+mod minimal {
+    use strict_num::FiniteF64;
+
+    #[derive(Debug, Clone, Copy)]
+    pub struct Vector<const N: usize> {
+        dims: [FiniteF64; N],
+    }
+    impl<const N: usize> Vector<N> {
+        #[must_use]
+        pub fn new(dims: [FiniteF64; N]) -> Self {
+            Self { dims }
+        }
+        #[must_use]
+        pub fn dims(&self) -> &[FiniteF64; N] {
+            &self.dims
+        }
+        #[must_use]
+        pub fn dims_mut(&mut self) -> &mut [FiniteF64; N] {
+            &mut self.dims
+        }
+        #[must_use]
+        pub fn zip_with(
+            &self,
+            other: &Self,
+            op: impl Fn(FiniteF64, FiniteF64) -> FiniteF64,
+        ) -> Self {
+            let dims = self
+                .dims
+                .iter()
+                .copied()
+                .zip(other.dims.iter().copied())
+                .map(|(a, b)| op(a, b))
+                .collect::<Vec<FiniteF64>>();
+            let dims = dims.try_into().unwrap();
+            Self { dims }
+        }
+    }
 }
+
 impl<const N: usize> Vector<N> {
     #[must_use]
-    pub fn new(dims: [FiniteF64; N]) -> Self {
-        Self { dims }
-    }
-    #[must_use]
-    pub fn dims(&self) -> &[FiniteF64; N] {
-        &self.dims
-    }
-
-    #[must_use]
     pub fn add(&self, other: &Self) -> Self {
-        let dims = self
-            .pairwise(other)
-            .map(|(a, b)| a.get() + b.get())
-            .map(|x| FiniteF64::new(x).unwrap())
-            .collect::<Vec<FiniteF64>>();
-        let dims = dims.try_into().unwrap();
-        Self { dims }
+        self.zip_with(other, |a, b| FiniteF64::new(a.get() + b.get()).unwrap())
     }
     #[must_use]
     pub fn sub(&self, other: &Self) -> Self {
-        let dims = self
-            .pairwise(other)
-            .map(|(a, b)| a.get() - b.get())
-            .map(|x| FiniteF64::new(x).unwrap())
-            .collect::<Vec<FiniteF64>>();
-        let dims = dims.try_into().unwrap();
-        Self { dims }
+        self.zip_with(other, |a, b| FiniteF64::new(a.get() - b.get()).unwrap())
     }
     #[must_use]
     pub fn dot(&self, other: &Self) -> f64 {
-        self.pairwise(other)
-            .map(|(a, b)| a.get() * b.get())
+        self.zip_with(other, |a, b| FiniteF64::new(a.get() * b.get()).unwrap())
+            .dims()
+            .map(|x| x.get())
+            .iter()
             .sum::<f64>()
     }
-    fn pairwise<'a>(
-        &'a self,
-        other: &'a Self,
-    ) -> impl Iterator<Item = (FiniteF64, FiniteF64)> + 'a {
-        self.dims.iter().copied().zip(other.dims.iter().copied())
-    }
     pub fn mul(&mut self, other: f64) {
-        self.dims
+        self.dims_mut()
             .iter_mut()
             .map(|x| (x.get() * other, x))
             .for_each(|(res, x)| *x = FiniteF64::new(res).unwrap());
@@ -64,7 +75,7 @@ impl<const N: usize> Vector<N> {
     }
     #[must_use]
     pub fn mag(&self) -> f64 {
-        let sum = self.dims.iter().map(|x| x.get().powi(2)).sum::<f64>();
+        let sum = self.dims().iter().map(|x| x.get().powi(2)).sum::<f64>();
         sum.sqrt()
     }
     pub fn normalize(&mut self) {
@@ -84,29 +95,25 @@ impl<const N: usize> Vector<N> {
     }
     #[must_use]
     pub fn lerp(&self, other: &Self, t: NormalizedF64) -> Self {
-        let dims = self
-            .pairwise(other)
-            .map(|(a, b)| lerp(&(a.get()..=b.get()), t.into()))
-            .map(|x| FiniteF64::new(x).unwrap())
-            .collect::<Vec<FiniteF64>>();
-        let dims = dims.try_into().unwrap();
-        Self { dims }
+        self.zip_with(other, |a, b| {
+            FiniteF64::new(lerp(&(a.get()..=b.get()), t.into())).unwrap()
+        })
     }
     #[must_use]
     pub fn heading_angle(&self, adjacent_axis: usize, opposite_axis: usize) -> f64 {
-        let adj = self.dims[adjacent_axis];
-        let opp = self.dims[opposite_axis];
+        let adj = self.dims()[adjacent_axis];
+        let opp = self.dims()[opposite_axis];
         f64::atan2(opp.get(), adj.get())
     }
     pub fn rotate(&mut self, adjacent_axis: usize, opposite_axis: usize, angle: f64) {
         let cos = angle.cos();
         let sin = angle.sin();
-        let x = self.dims[adjacent_axis];
-        let y = self.dims[opposite_axis];
+        let x = self.dims()[adjacent_axis];
+        let y = self.dims()[opposite_axis];
         let x_ = cos * x.get() + -sin * y.get();
         let y_ = sin * x.get() + cos * y.get();
-        self.dims[adjacent_axis] = FiniteF64::new(x_).unwrap();
-        self.dims[opposite_axis] = FiniteF64::new(y_).unwrap();
+        self.dims_mut()[adjacent_axis] = FiniteF64::new(x_).unwrap();
+        self.dims_mut()[opposite_axis] = FiniteF64::new(y_).unwrap();
     }
     #[must_use]
     pub fn angle_between(&self, other: &Self) -> f64 {
@@ -140,19 +147,22 @@ impl Vector<3> {
     pub fn cross(&self, other: &Self) -> Self {
         let dims = [
             FiniteF64::new(
-                self.dims[1].get() * other.dims[2].get() - self.dims[2].get() * other.dims[1].get(),
+                self.dims()[1].get() * other.dims()[2].get()
+                    - self.dims()[2].get() * other.dims()[1].get(),
             )
             .unwrap(),
             FiniteF64::new(
-                self.dims[2].get() * other.dims[0].get() - self.dims[0].get() * other.dims[2].get(),
+                self.dims()[2].get() * other.dims()[0].get()
+                    - self.dims()[0].get() * other.dims()[2].get(),
             )
             .unwrap(),
             FiniteF64::new(
-                self.dims[0].get() * other.dims[1].get() - self.dims[1].get() * other.dims[0].get(),
+                self.dims()[0].get() * other.dims()[1].get()
+                    - self.dims()[1].get() * other.dims()[0].get(),
             )
             .unwrap(),
         ];
-        Self { dims }
+        Self::new(dims)
     }
 }
 impl<const N: usize> From<Vector<N>> for matrix::ArrayMatrixBuf<f64, N> {
@@ -161,7 +171,7 @@ impl<const N: usize> From<Vector<N>> for matrix::ArrayMatrixBuf<f64, N> {
             rows: NonZeroUsize::new(N).unwrap(),
             cols: NonZeroUsize::new(1).unwrap(),
         };
-        let data = value.dims.map(|x| x.get());
+        let data = value.dims().map(|x| x.get());
         matrix::ArrayMatrixBuf::new(size, data)
     }
 }
@@ -200,7 +210,7 @@ mod tests {
         let a = Vector::new([0., 1.].map(|x| FiniteF64::new(x).unwrap()));
         let b = Vector::new([1., 2.].map(|x| FiniteF64::new(x).unwrap()));
         let c = a.add(&b);
-        assert_eq!(c.dims, [1., 3.].map(|x| FiniteF64::new(x).unwrap()));
+        assert_eq!(*c.dims(), [1., 3.].map(|x| FiniteF64::new(x).unwrap()));
     }
 
     #[test]
@@ -219,7 +229,7 @@ mod tests {
     fn test_rotation() {
         let mut v = Vector::new([1., 0.5].map(|x| FiniteF64::new(x).unwrap()));
         v.rotate_2d(PI / 2.);
-        assert!(v.dims[0].get().closes_to(-0.5));
-        assert!(v.dims[1].get().closes_to(1.));
+        assert!(v.dims()[0].get().closes_to(-0.5));
+        assert!(v.dims()[1].get().closes_to(1.));
     }
 }
